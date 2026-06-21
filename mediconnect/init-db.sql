f0070402bd5550d90d82c36d08ff04e92ae727b7
Parthiban Ramasamy <ramasamyparthiban16@gmail.com>
2026-06-21 19:24:30 +0530

    Docker complete - all services containerized and tested

---
+++ mediconnect/init-db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS mediconnect_users;
+CREATE DATABASE IF NOT EXISTS mediconnect_doctors;
+CREATE DATABASE IF NOT EXISTS mediconnect_appointments;
+CREATE DATABASE IF NOT EXISTS mediconnect_prescriptions;
+CREATE DATABASE IF NOT EXISTS mediconnect_payments;
